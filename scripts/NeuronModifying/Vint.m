619aacd2011b6514b6ea3f5b1a5f73ff99aa8495
Julian Smith <jsmith767@gmail.com>
2021-09-07 13:03:44 -0700

    Corrected Vint (general)

--- scripts/NeuronModifying/Vint.m
+++ scripts/NeuronModifying/Vint.m
@@ -1,10 +1,18 @@
-% method = 'allangles';
-method = 'W';
-for i=1:length(masterN)
-    i
-%     [~,~,~,masterN(i).F,~] = analyzeNeuron(masterN(i).path,masterN(i).name);
-    for j=1:9
-        Vinttemp =makeSurface(vertcat(masterN(i).data.(genvarname(method))(j).VAll{:}),vertcat(masterN(i).F{:}),0.1);
-        masterN(i).data.(genvarname(method))(j).VintR1 =unique(round(Vinttemp),'rows');
+function masterN = Vint(masterN,varargin)
+p = inputParser;
+addParameter(p,'method','F',@ischar);
+addParameter(p,'resolution',0.1,@isnumeric);
+addParameter(p,'round',1,@isnumeric); %set the division you want to round to e.g. 2 -> 0.5 4 -> 0.25
+parse(p,varargin{:});
+method = p.Results.method;
+    % method = 'allangles';
+%     method = 'W';
+    for i=1:length(masterN) %for all the neurons
+%         i
+    %     [~,~,~,masterN(i).F,~] = analyzeNeuron(masterN(i).path,masterN(i).name);
+        for j=1:length(masterN(i).data.(genvarname(method))) % for all the curvatures
+            Vinttemp =makeSurface(vertcat(masterN(i).data.(genvarname(method))(j).VAll{:}),vertcat(masterN(i).F{:}),p.Results.resolution);
+            masterN(i).data.(genvarname(method))(j).(genvarname(['VintR' num2str(p.Results.round)])) = unique(round(Vinttemp * (p.Results.round)) / (p.Results.round),'rows');
+        end
     end
 end
